14740fca66b72af3fb6a8d8e3c5d12aece6b71ff
diego-alvesss <diego.g.alves8@aluno.senai.br>
2025-09-19 12:59:07 -0300

    gps incluse

--- intro_lutter/lib/pages/home_screen.dart
+++ intro_lutter/lib/pages/home_screen.dart
@@ -2,6 +2,8 @@ import 'package:flutter/material.dart';
 import 'package:intro_lutter/components/custom_card.dart';
 import 'package:intro_lutter/exemplos/Value_notifier_counter_screen.dart';
 import 'package:intro_lutter/exemplos/changed_notifier.dart';
+import 'package:intro_lutter/pages/gps_page.dart';
+
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
@@ -33,6 +35,13 @@ class HomeScreen extends StatelessWidget {
             description: 'Exemplo com ChangedNotifier',
             icon: Icons.change_circle_outlined,
             destination: SimpleChangeNotifierExample(),
+          ),
+          SizedBox(height: 16),
+          CustomCard(
+            title: 'GPS',
+            description: 'Localização e Mapa',
+            icon: Icons.location_on,
+            destination: GpsPage(),
           ),
                   ],
                 ),
